ea2325ffc84288af9d3080cb250a518794055d3e
Theophilus Samuel <36473098+ConnectedDot@users.noreply.github.com>
2024-09-16 22:29:51 +0100

    Fixed

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -7,8 +7,6 @@ import {
 	setStoredUser,
 } from "../storage";
 import {ChildProps, instructorProps, userProps} from "../interface";
-import {ChildProps, userProps} from "../interface";
-import {useObservingUser} from "./hooks";
 
 export const AuthContext = createContext({
 	user: undefined as userProps | undefined,
@@ -24,27 +22,7 @@ function AuthContextProvider({children}: ChildProps) {
 	const [user, setUser] = useState<userProps | undefined>(undefined);
 	const [fireUser, setFireUser] = useState<userProps | undefined>(undefined);
 	const [cart, setCart] = useState<userProps[]>([]);
-
-	//trying to refcth userupdateddata frequesntly
-
-	// useEffect(() => {
-	// 	const fetchUserData = async () => {
-	// 		try {
-	// 			const data = await getCurrentUserData();
-	// 			// setUser(data as any);
-	// 			console.log(data, "Data im refreshing");
-	// 		} catch (err: any) {
-	// 			// setError(err.message);
-	// 			console.log(err);
-	// 		}
-	// 	};
-
-	// 	fetchUserData();
-	// }, []);
-
-	const [databaseUsers, users] = useObservingUser();
-	console.log(databaseUsers, "Data from firestore");
-	console.log(users, "userdata from firebase");
+	const userDetails = getStoredUser();
 
 	useEffect(() => {
 		const data = getLoginToken();
