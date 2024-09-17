83e280d08bfccd719598212fd7a044f8896b8ba1
Theophilus Samuel <36473098+ConnectedDot@users.noreply.github.com>
2024-09-17 02:29:30 +0100

    Refactor AvatarMenu component to remove commented out code and unused props

--- src/layout/Instructor/Navbar/AvatarMenu.tsx
+++ src/layout/Instructor/Navbar/AvatarMenu.tsx
@@ -139,7 +139,7 @@ export const AvatarMenu = () => {
 									<button
 										className="block w-full items-start px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
 										onClick={dropdownItems[dropdownItems.length - 1].onClick}
-										// to="#"
+
 										// onClick={useFullLogout}
 									>
 										{dropdownItems[dropdownItems.length - 1].name}
